8d285bfae89c8fdfb6f4258cca677dac0631ba48
yesthis1please <yesthis1please@gmail.com>
2022-09-05 21:43:44 +0900

    Set up the static look

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>hello there</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h3>0 birthday today</h3>
+        <List />
+        <button onClick={() => console.log("you clicked me")}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
