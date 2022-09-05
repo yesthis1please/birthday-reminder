be5034fdbbccd5fe3aa67787fe956b9b4fd6cfd2
yesthis1please <yesthis1please@gmail.com>
2022-09-05 21:52:06 +0900

    Added data to the static look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main>
       <section className="container">
-        <h3>0 birthday today</h3>
-        <List />
+        <h3>{people.length} birthday today</h3>
+        <List people={people}/>
         <button onClick={() => console.log("you clicked me")}>clear all</button>
       </section>
     </main>
